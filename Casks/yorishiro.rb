56e1558b758468dcbff58eeef6b3fe2f44cd2ba6
okktks <ookaki7@gmail.com>
2026-08-08 14:20:12 +0900

    yorishiro 0.7.0

--- Casks/yorishiro.rb
+++ Casks/yorishiro.rb
@@ -5,15 +5,15 @@
 # Yorishiro_* / Yorishiro.app に変わる。この変更は v0.7.0 の公開後、
 # version / sha256 の bump と同時に main へ反映すること。
 cask "yorishiro" do
-  version "0.6.2"
+  version "0.7.0"
 
   on_arm do
-    sha256 "ffeea38563f6ca04261fafdfc0c97e8ab37806307a3d5e645c088c9d91c492e1"
+    sha256 "1aa9755cde1438d61fd022db9e320f7c649f2bd195fd4bd46da926dea46087b7"
 
     url "https://github.com/sktkkoo/Yorishiro/releases/download/v#{version}/Yorishiro_#{version}_aarch64.dmg"
   end
   on_intel do
-    sha256 "8289b215e04ea4686fe8f7a8c95dff24284f8d0a3284fb3206b359fa7d5d8645"
+    sha256 "3f390f2bc3811dfd2c315de01bcff09f356ce51c6a06b81fd87b4cb775c2c5ba"
 
     url "https://github.com/sktkkoo/Yorishiro/releases/download/v#{version}/Yorishiro_#{version}_x64.dmg"
   end
